78cfacae8bdbcdd228b75bdbdc01f090d241a38e
Nush <66404771+NusheenBari@users.noreply.github.com>
2023-03-03 00:07:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -9,6 +10,17 @@ export default function Home() {
         <title>Career Coach</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+    
+    <header>
+        <nav>
+          <Image
+              src='/Career_Coach_logo.png'
+              alt='Career Coach'
+              width={200}
+              height={80} 
+              />
+          </nav>
+        </header>
 
       <main>
         <Header title="Are you Looking to Advance Your Career?" />
@@ -26,6 +38,21 @@ export default function Home() {
                 <li>Career Development</li>
               </ul>
             </section>
+
+ <article>
+                <a className='py-3 px-3' href="https://www.linkedin.com/in/career-coach-2299b0264">
+                    <Image  src="/icons8-linkedin-circled.svg"
+                    alt = "LinkedIn"
+                    width={30}
+                    height={30}
+                    />
+                  </a>
+
+                  <a href="tel:08197410859">
+                    <button className='py-3 px-6 bg-blue-900 text-white font-medium rounded-md'>Contact us</button>
+                  </a>
+            </article>
+
       </main>
 
       <Footer />
